fb78185694c0196c1f350d2c1b941891496ac537
Fabio Magnoni <fabiohmagnoni@gmail.com>
2015-07-03 00:41:35 -0300

    adicionando site INTV

---
+++ include/carrousel.php
@@ -0,0 +1,49 @@
+﻿<div id="loading" class="index-blurb"></div>
+<div class="giant-carousel is-index" data-widget="giant-carousel" data-gc-timer="5000" >
+
+		<div class="giant-carousel__pagination" style="top: 341px;">
+			<span class="giant-carousel__pagination-text" data-slides="2">0 / 0</span>
+			<div class="giant-carousel__pagination-buttons">
+				<a href="#" data-push="ignore" class="giant-carousel__pagination-prev" data-analytics="button|click|Homepage - Giant Carousel - Previous|true" data-analytics-count="0">Prev</a>
+				<a href="#" data-push="ignore" class="giant-carousel__pagination-next" data-analytics="button|click|Homepage - Giant Carousel - Next|true" data-analytics-count="0">Next</a>
+			</div>
+		</div>
+
+
+
+
+		<ul class="giant-carousel__list" style="height: 702px; margin:-25px 0 0 0">
+
+			<li class="giant-carousel__list-item" style="background-image: url(http://intv.tv.br/off/images/cristo1.jpg);">
+				<span class="giant-carousel__item-caption">Central do Cristo</span>
+				<a href="#" class="giant-carousel__item-link" data-push="true">Ver Projeto</a>
+			</li>
+
+			<li class="giant-carousel__list-item" style="background-image: url(http://intv.tv.br/off/images/wifi-users.png);">
+				<span class="giant-carousel__item-caption">INTV Wi-Fi</span>
+				<a href="#" class="giant-carousel__item-link" data-push="true">Ver Projeto</a>
+			</li>
+			<li class="giant-carousel__list-item" data-kiu="mobile-switch" data-desktop-image="http://intv.tv.br/off/images/slider01.jpg" data-mobile-image="http://intv.tv.br/off/images/slider01.jpg" data-tablet-image="http://intv.tv.br/off/images/slider01.jpg" data-kiu-mode="desktop" style="background-image: url(http://intv.tv.br/off/images/slider01.jpg);">
+				<span class="giant-carousel__item-caption">Propriedades de Imagem</span>
+				<a href="#" class="giant-carousel__item-link" data-push="true">Ver Projeto</a>
+			</li>
+			<li class="giant-carousel__list-item" data-kiu="mobile-switch" data-desktop-image="http://intv.tv.br/off/images/slider02.jpg" data-mobile-image="http://intv.tv.br/off/images/slider01.jpg" data-tablet-image="http://intv.tv.br/off/images/slider01.jpg" data-kiu-mode="desktop" style="background-image: url(http://intv.tv.br/off/images/slider01.jpg);">
+				<span class="giant-carousel__item-caption">Social Media</span>
+				<a href="#" class="giant-carousel__item-link" data-push="true">Ver Projeto</a>
+			</li>
+
+		</ul>
+
+		<div class="contain">
+			<div class="giant-carousel__caption-container quarter" style="-webkit-animation-delay: 0s; -webkit-animation-duration: 0.5s; -webkit-animation-direction: normal; -webkit-animation-fill-mode: both; -webkit-animation-timing-function: ease;">
+				<span class="giant-carousel__caption" style="display: block; -webkit-animation-delay: 0s; -webkit-animation-duration: 0.5s; -webkit-animation-direction: normal; -webkit-animation-fill-mode: both; -webkit-animation-timing-function: ease;">Social Media						</span>
+				<a href="#" class="giant-carousel__caption-link" data-push="true">Ver Projeto</a>
+
+				<a href="#" class="giant-carousel__caption-link-cover" data-analytics="button|click|Homepage - Landing - View Project" data-analytics-count="0" data-push="true">View Project</a>
+
+				<div class="giant-carousel__caption-hover"></div>
+			</div>
+		</div>
+
+		<div class="clearfix"></div>
+	</div>
