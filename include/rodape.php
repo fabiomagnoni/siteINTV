7b7f2bc010f8c157670889533faa1955665ab27d
Fabio Magnoni <fabiohmagnoni@gmail.com>
2015-07-10 17:35:19 -0300

    remoção da home

--- include/rodape.php
+++
@@ -1,4 +0,0 @@
-﻿	<script src="jquery/js"></script><script src="jquery/main(1).js"></script>
-	<script src="jquery/plugins.js" defer></script>
-	<script data-main="http://www.firstborn.com/assets/scripts/min/main" src="jquery/require.js" defer></script>
-	<link rel="stylesheet" async="" type="text/css" href="css/mobile.css">
